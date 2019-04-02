eb34193c9cbb9523cdcca63646cc0096179ba1bd
Daniel Perez Goti <Daniel.Perez@ricoh.es>
2019-04-02 13:00:26 +0200

    Commit version tabla basica

--- src/main/java/com/practices/demo/SpringDemoApplication.java
+++ src/main/java/com/practices/demo/SpringDemoApplication.java
@@ -1,23 +1,13 @@
 package com.practices.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.practices.demo.persistence.PersonaRepository;
 
 @SpringBootApplication
 public class SpringDemoApplication {
 	
-	@Autowired
-	private static PersonaRepository repositorio;
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDemoApplication.class, args);
-		
-		repositorio.findAll().forEach((p)->System.out.println(p.getName()));
-		
+		SpringApplication.run(SpringDemoApplication.class, args);		
 	}
-	
 	
 }
