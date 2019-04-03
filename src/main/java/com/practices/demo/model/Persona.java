89eabe5a4f2a767cad6d4300bf00d726b419453b
Daniel Perez Goti <Daniel.Perez@ricoh.es>
2019-04-03 13:17:54 +0200

    Commit actualizado botón create person

--- src/main/java/com/practices/demo/model/Persona.java
+++ src/main/java/com/practices/demo/model/Persona.java
@@ -1,6 +1,8 @@
 package com.practices.demo.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
