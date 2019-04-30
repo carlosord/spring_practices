a331ba7b3db458b6f77812aac1287306a7017e54
Jairo Rodriguez De La Vallina <Jairo.Rodriguez@ricoh.es>
2019-04-30 10:09:32 +0200

    [005] Create car repository

---
+++ src/main/java/com/practices/demo/repositories/CarRepository.java
@@ -0,0 +1,15 @@
+package com.practices.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.practices.demo.model.Car;
+
+/**
+ * The repository of car
+ */
+
+@Repository
+public interface CarRepository extends JpaRepository<Car, Long> {
+
+}
