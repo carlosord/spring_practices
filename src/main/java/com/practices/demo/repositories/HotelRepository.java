f6b430de935f0df3e2e3a1343e974ff2a2b6b7d8
Daniel Perez Goti <Daniel.Perez@ricoh.es>
2019-04-30 09:58:39 +0200

    [006] Fix Create Hotel Repository

--- src/main/java/com/practices/demo/repositories/HotelRepository.java
+++ src/main/java/com/practices/demo/repositories/HotelRepository.java
@@ -1,16 +1,14 @@
 package com.practices.demo.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.practices.demo.model.Hotel;
 
+/**
+ *  The repository of Hotel
+ */
 @Repository
 public interface HotelRepository extends JpaRepository<Hotel,Long>{
-	
-	@Query("SELECT h from Hotel h WHERE h.code=(:code)")
-	Hotel findByCode(@Param("code")int code); 
 
 }
