8bc91e9172cc4d36cc865827c19f0bca4951c202
Jairo Rodriguez De La Vallina <Jairo.Rodriguez@ricoh.es>
2019-05-23 13:14:07 +0200

    [026] create new properties for email and fix errors

--- src/main/java/com/practices/demo/SpringDemoApplication.java
+++ src/main/java/com/practices/demo/SpringDemoApplication.java
@@ -2,12 +2,18 @@ package com.practices.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources({
+    @PropertySource("classpath:application.properties"),
+    @PropertySource("classpath:application_config_mail.properties")
+})
 public class SpringDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
-	
+
 }
