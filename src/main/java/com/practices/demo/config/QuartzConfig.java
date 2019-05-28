6e44dbb8b700d511f93ef4844dc3625f6d64f58a
Daniel Perez Goti <Daniel.Perez@ricoh.es>
2019-05-28 16:40:31 +0200

    [033] Save Changes

---
+++ src/main/java/com/practices/demo/config/QuartzConfig.java
@@ -0,0 +1,29 @@
+package com.practices.demo.config;
+
+import org.quartz.CronScheduleBuilder;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.practices.demo.service.impl.QuartzServiceImpl;
+
+@Configuration
+public class QuartzConfig {
+
+	@Bean
+	public JobDetail jobEmail() {
+		return JobBuilder.newJob(QuartzServiceImpl.class).withIdentity("sendEmailJob").storeDurably().build();
+	}
+
+	@Bean
+	public Trigger emailTrigger(JobDetail jobADetails) {
+
+		return TriggerBuilder.newTrigger().forJob(jobADetails)
+
+				.withIdentity("sendEmailTrigger").withSchedule(CronScheduleBuilder.cronSchedule("0/30 0/1 * 1/1 * ? *")).build();
+	}
+
+}
--- src/main/java/com/practices/demo/service/impl/MailServiceImpl.java
+++ src/main/java/com/practices/demo/service/impl/MailServiceImpl.java
@@ -1,9 +1,20 @@
 package com.practices.demo.service.impl;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.practices.demo.model.Person;
@@ -19,6 +30,7 @@ public class MailServiceImpl implements MailService {
 	@Autowired
 	private JavaMailSender javaMailSender;
 
+
 	/**
 	 * Send email.
 	 *
@@ -30,11 +42,42 @@ public class MailServiceImpl implements MailService {
 		SimpleMailMessage mail = new SimpleMailMessage();
 
 		mail.setTo("pepepaquetemetes123@gmail.com");
-		mail.setSubject("SpringBoot Reserve");
+		mail.setSubject(("mail.subject.reserve"));//CAMBIAR
 		mail.setText(person.getName() + ", your reservation has been made successfully");
 
 		javaMailSender.send(mail);
 
+	}
+
+	/**
+	 * Send daily email.
+	 */
+	public void sendDailyEmail() {
+		hourNow();
+		try {
+			MimeMessage message = javaMailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setTo("pepepaquetemetes123@gmail.com");
+			helper.setSubject(("mail.subject.day"));//CAMBIAR
+			helper.setText("Testing messages");
+			// helper.addAttachment("MyPdf.pdf", new FileSystemResource("F:\\Doc\\index.pdf"));
+
+			this.javaMailSender.send(message);
+
+
+		} catch (MessagingException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+	}
+
+	/**
+	 * Hour now.
+	 */
+	private void hourNow() {
+		DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+		Date date = new Date();
+		System.out.println("Email sent at " + dateFormat.format(date));
 	}
 
 }
